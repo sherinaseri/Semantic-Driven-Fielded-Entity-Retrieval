d728f4493a042a7475cf932329d0f8eda85c390d
Shahrzad Naseri <shnaseri@bendigo.cs.umass.edu>
2018-08-21 20:32:06 -0400

    clean directory

--- INEX_LD/script.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-cat fold0/fsdm.run fold1/fsdm.run fold2/fsdm.run fold3/fsdm.run fold4/fsdm.run > fsdm.run
-cat fold0/entity_semantic.run fold1/entity_semantic.run fold2/entity_semantic.run fold3/entity_semantic.run fold4/entity_semantic.run > entity_semantic.run
-cat fold0/term_semantic.run fold1/term_semantic.run fold2/term_semantic.run fold3/term_semantic.run fold4/term_semantic.run > term_semantic.run
-cat fold0/semantic.run fold1/semantic.run fold2/semantic.run fold3/semantic.run fold4/semantic.run > semantic.run
-
